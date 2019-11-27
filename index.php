9ea3bd5cfac0d8bac612b48d85d061a2244a33f8
Fabian Vergara <vergarf@gmail.com>
2019-11-27 16:47:04 -0500

    Finished index (initial template) plus styling

--- index.php
+++ index.php
@@ -1,4 +1,52 @@
-<?php
-echo "Hello From Sites Folder!";
-phpinfo();
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="./Styles/main.css">
+</head>
+<body>
+
+<div class="header">
+  <h1>The SCC-Network</h1>
+</div>
+
+<div class = "row welcome-nav-bar">
+    <div class="col-8">
+        Welcome, {username}!
+    </div>
+    <div class="col-2 no-padding">
+        <button class="welcome-nav-bar-btn">Profile</button>
+    </div>
+    <div class="col-2 no-padding">
+        <button class="welcome-nav-bar-btn">Messages</button>
+    </div>
+</div>
+
+<div class="row">
+  <div class="col-2 menu">
+    <ul>
+    <li>Home page</li>
+    <li>My events</li>
+    <li>My groups</li>
+    <li>Event manager panel </li>
+    <li>Admin panel</li>
+    <li>Controller panel</li>
+    </ul>
+  </div>
+
+  <div class="col-8">
+    <h1>Share, Contribute, Comment... Connect. The Network.</h1>
+    <p><b>Sign up now</b> to be part of the <b>overgrowing community</b> of participants in a whole variety of events. </br> </br>
+        <b>Become</b> a member of the family, <b>choose</b> whether to participate in an event or organize a new inedit one! <b>Join</b> existing groups in their respective events
+        composed by other participants or <b>be</b> the sole all mighty by creating your own! </br>
+        <b>Comment</b>, <b>contribute</b> and <b>share</b> content, and chat with other people alike sharing your same own interests and assisting your same own events! </br></br></br>
+        
+        <b><i>Because great minds think alike.</i></b>
+    
+    </p>
+  </div>
+</div>
+
+</body>
+</html>
+
